434f73a7db2b5b0fd4b0e6a45462aae1c6757d3c
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-16 05:14:50 -0400

    server working but not env yet

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+
+server.use(express.json());
+server.use(helmet());
+
+
+module.exports = server;
+
+
